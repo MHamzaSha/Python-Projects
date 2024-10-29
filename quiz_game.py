80c055dbe8747735a3c06498a6b40f2bcb75850c
HamzaShakeel <mmhamza672@gmail.com>
2024-10-29 23:46:24 +0500

    add first question

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,15 @@
-print("hello")
+print("Welcome to my computer quiz")
+
+playing = input("Do you want to play the game? ")
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play : )")
+
+answer =input("What does CPU stand for? ")
+if answer in ("central processing unit","Central processsing unit") :
+    print ("Correct!")
+else:
+    print("Incorrrect!")
+
+
