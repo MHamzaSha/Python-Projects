77df3f1be315ee4ad031b7682362dde85758549f
HamzaShakeel <mmhamza672@gmail.com>
2024-10-30 15:46:07 +0500

    adding all the questios

--- quiz_game.py
+++ quiz_game.py
@@ -6,10 +6,38 @@ if playing != "yes":
 
 print("Okay! Let's play : )")
 
+#1st
 answer =input("What does CPU stand for? ")
-if answer in ("central processing unit","Central processsing unit") :
+if answer in ("central processing unit","Central processing unit") :
     print ("Correct!")
 else:
     print("Incorrrect!")
 
+#2nd
+answer =input("What does GPU stand for? ")
+if answer in ("graphics processing  unit","Graphics processsing unit") :
+    print ("Correct!")
+else:
+    print("Incorrrect!")
+
+# #3rd
+# answer =input("What does CPU stand for? ")
+# if answer in ("central processing unit","Central processsing unit") :
+#     print ("Correct!")
+# else:
+#     print("Incorrrect!")
+
+# #4th
+# answer =input("What does CPU stand for? ")
+# if answer in ("central processing unit","Central processsing unit") :
+#     print ("Correct!")
+# else:
+#     print("Incorrrect!")
+
+# #5th
+# answer =input("What does CPU stand for? ")
+# if answer in ("central processing unit","Central processsing unit") :
+#     print ("Correct!")
+# else:
+#     print("Incorrrect!")
 
