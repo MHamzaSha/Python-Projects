71e8c5511d1ca74842f1e4024629a3bce6eb26da
HamzaShakeel <mmhamza672@gmail.com>
2024-10-30 18:29:27 +0500

    add score program

--- quiz_game.py
+++ quiz_game.py
@@ -1,43 +1,51 @@
 print("Welcome to my computer quiz")
 
 playing = input("Do you want to play the game? ")
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play : )")
+score=0
 
 #1st
 answer =input("What does CPU stand for? ")
-if answer in ("central processing unit","Central processing unit") :
+if answer.lower() == ("central processing unit") :
     print ("Correct!")
+    score+=1
 else:
     print("Incorrrect!")
 
 #2nd
 answer =input("What does GPU stand for? ")
-if answer in ("graphics processing  unit","Graphics processsing unit") :
+if answer.lower() == ("graphics processing unit") :
     print ("Correct!")
+    score+=1
 else:
     print("Incorrrect!")
 
-# #3rd
-# answer =input("What does CPU stand for? ")
-# if answer in ("central processing unit","Central processsing unit") :
-#     print ("Correct!")
-# else:
-#     print("Incorrrect!")
+#3rd
+answer =input("What does ROM stand for? ")
+if answer.lower() == ("read only memory") :
+    print ("Correct!")
+    score+=1
+else:
+    print("Incorrrect!")
 
-# #4th
-# answer =input("What does CPU stand for? ")
-# if answer in ("central processing unit","Central processsing unit") :
-#     print ("Correct!")
-# else:
-#     print("Incorrrect!")
+#4th
+answer =input("What does PSU stand for? ")
+if answer.lower() == ("power supply unit") :
+    print ("Correct!")
+    score+=1
+else:
+    print("Incorrrect!")
 
-# #5th
-# answer =input("What does CPU stand for? ")
-# if answer in ("central processing unit","Central processsing unit") :
-#     print ("Correct!")
-# else:
-#     print("Incorrrect!")
+#5th
+answer =input("What does RAM stand for? ")
+if answer.lower() == ("random access memory") :
+    print ("Correct!")
+    score+=1
+else:
+    print("Incorrrect!")
 
+print("You got "+str(score)+" questions correct.")
+print ("You got"+str((score/5)*100)+ "%.")    
