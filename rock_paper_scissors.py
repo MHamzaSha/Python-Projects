b37a6c5f86299ac4df2f232d7dd482ffe1728c5d
HamzaShakeel <mmhamza672@gmail.com>
2024-10-31 21:00:09 +0500

    Complete Rock-Paper-Scissors game logic with win conditions and score display
    - Added random selection for computer’s choice using a list and random number.
    - Defined win conditions for user based on their input against computer's pick.
    - Updated user and computer win counters after each round.
    - Displayed total wins for both user and computer at the end of the game.
    - Added farewell message when the game is exited.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,11 +2,40 @@ import random
 
 user_wins=0
 computer_wins=0
+options = ["rock","paper","scissors"]
 
 while True:
     user_input=input("Type Rock/Paper/Scissors or Q to quit. : ").lower()
     if user_input == "q":
         break
-    if user_input not in ["rock","paper","scissors"]:
+    if user_input not in options:
         continue
+    random_num=random.randint(0,2)
+    copmuter_pick =options[random_num]
+    print("Computer picked",copmuter_pick+".")
+
+    if user_input == "rock" and copmuter_pick =="scissors":
+        print("You Won!")
+        user_wins+=1
+
+    elif user_input == "paper" and copmuter_pick =="rock":
+        print("You Won!")
+        user_wins+=1
+
+    elif user_input == "scissors" and copmuter_pick =="paper":
+        print("You Won!")
+
+    else:
+        print("You Lost!")
+        computer_wins+=1
+
+        user_wins+=1    
+
+print("You won",user_wins,"times.")
+print("Computer won",computer_wins,"times.")
+
+
+
+print("Goodbye!")
+
 
